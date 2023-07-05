3aff463538caed1fe6e17d30ea8e76708eab0da4
Michele Locati <michele@locati.it>
2023-07-05 14:54:07 +0200

    Instructions moved to marketplace page.

--- single_pages/dashboard/system/environment/telegram-errors.php
+++ single_pages/dashboard/system/environment/telegram-errors.php
@@ -73,34 +73,16 @@ defined('C5_EXECUTE') or die('Access Denied.');
     <div class="form-group">
         <?= $form->label('tg_token', t('Telegram token')) ?>
         <?= $form->password('tg_token', $tg_token) ?>
+        <div class="small text-muted"><?= t('For detailed instructions %ssee here%s.', '<a href="https://marketplace.concretecms.com/marketplace/addons/notify-via-telegram/getting-telegram-tokens-and-telegram-recipients/" target="_blank">', '</a>') ?></div>
     </div>
 
     <div class="form-group">
         <?= $form->label('tg_recipients', t('Telegram recipients')) ?>
         <?= $form->textarea('tg_recipients', $tg_recipients === [] ? '' : (implode("\n", $tg_recipients) . "\n")) ?>
-        <div class="small text-muted"><?= t('Separate multiple recipients with spaces or new lines') ?></div>
-    </div>
-
-    <div class="alert alert-info">
-        <h4><?= t('Instructions') ?></h4>
-        <ol>
-            <li>
-                <?= t('Create a new bot with the %1$s command of the %2$s Telegram bot', '<code>/newbot</code>', '<code><a href="https://t.me/BotFather" target="_blank">@BotFather</a></code>') ?>
-                <br />&nbsp;
-            </li>
-            <li>
-                <?= t('Paste above the generated token') ?>
-                <br />&nbsp;
-            </li>
-            <li>
-                <?= t('Create a new Telegram group and add to it your newly created bot') ?>
-                <br />&nbsp;
-            </li>
-            <li>
-                <?= t(/*i18n: %s is "chat ID" */'You need to know the value of %s that identifies the Telegram group.', '<code>chat ID</code>') ?><br />
-                <?= t('The easiest way is to temporarily add the %s Telegram bot the group: it will display the technical details of it.', '<code><a href="https://t.me/RawDataBot" target="_blank">@RawDataBot</a></code>') ?><br />
-                <?= t('Locate the %1$s section: you can paste the value of the %2$s field in the recipients field above.', '<code>message</code> &rarr; <code>chat</code>', '<code>id</code>')?>
-        </ol>
+        <div class="small text-muted">
+            <?= t('Separate multiple recipients with spaces or new lines.') ?>
+            <?= t('For detailed instructions %ssee here%s.', '<a href="https://marketplace.concretecms.com/marketplace/addons/notify-via-telegram/getting-telegram-tokens-and-telegram-recipients/" target="_blank">', '</a>') ?>
+        </div>
     </div>
 
     <div class="ccm-dashboard-form-actions-wrapper">
